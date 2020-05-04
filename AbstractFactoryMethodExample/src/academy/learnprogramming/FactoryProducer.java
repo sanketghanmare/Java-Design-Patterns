c7114f0d6d35ece5d1e579eee7f7a330362a20c6
Sanket Ghanmare <sanketghanmare@gmail.com>
2020-05-03 22:56:41 -0400

    adding sample abstractfactoryexample

---
+++ AbstractFactoryMethodExample/src/academy/learnprogramming/FactoryProducer.java
@@ -0,0 +1,17 @@
+package academy.learnprogramming;
+
+public class FactoryProducer {
+    public static AbstractFactory getFactory(String choice)
+    {
+
+        if(choice.equalsIgnoreCase("SHAPE")){
+            return new ShapeFactory();
+
+        }else if(choice.equalsIgnoreCase("COLOR")){
+            return new ColorFactory();
+        }
+
+        return null;
+    }
+
+}
